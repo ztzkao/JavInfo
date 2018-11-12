dc6f4779071a4e4aed7beb63d5d9bdeac8cae1ec
ztzkao <ztzkao@gmail.com>
2018-11-12 15:01:05 +0800

    Initial commit

---
+++ site/caribbeancom.py
@@ -0,0 +1,35 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def getactor_cn(soup):
+    fullname = ''
+    for name in soup.find_all(name='span', itemprop="name"):
+        fullname += name.string + ' '
+    return fullname
+
+def getcat_cn(soup):
+    cates = ''
+    for name in soup.find_all(name='dl', class_='movie-info-cat'):
+        for span in name.find_all(name='span'):
+            cates += span.string + ' '
+        for a in name.find_all(name='a', itemprop = 'genre'):
+            cates += a.string + ' '
+    return cates
+
+def getuploadDate_cn(soup):
+    date = soup.find(name='dd', itemprop="uploadDate").string
+    return date
+
+def getduration_cn(soup):
+    get = soup.find(name='span', itemprop="duration").string
+    duration = get.replace(' ', '')
+    return duration
+
+id = input('输入id:\n')
+r = requests.get('https://cn.caribbeancom.com/moviepages/' + id + '/index.html')
+soup = BeautifulSoup(r.text, 'lxml')
+print(getcat_cn(soup)+ '\n')
+print(getactor_cn(soup) + '\n')
+print(getuploadDate_cn(soup) + '\n')
+print(str(getduration_cn(soup)) + '\n')
