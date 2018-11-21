39a266dadd420b5ba95b23e033e5a5df8d85644d
ztzkao <ztzkao@gmail.com>
2018-11-21 19:14:02 +0800

    combined

--- JavInfo.py
+++ JavInfo.py
@@ -1,3 +1,5 @@
+import requests
+import os
 from sites import caribbeancom
 from sites import caribbeancompr
 from sites import heyzo
@@ -20,8 +22,24 @@ siteDict = {
 
 def getInfo(id, siteNum):
     data = eval(str(siteDict[siteNameList[int(siteNum)]]))
+    saveImage(data)
     return data
-    
+
+def saveImage(item):
+    if not os.path.exists('cover'):
+        os.mkdir('cover')
+    try:
+        response = requests.get(item['coverLink'])
+        if response.status_code == 200:
+            file_path = "{0}/{1}.{2}".format('cover',item['id'], 'jpg')
+            if not os.path.exists(file_path):
+                with open(file_path, 'wb') as f:
+                    f.write(response.content)
+                    item['coverPATH'] = file_path
+            else:
+                print('Already Downloaded', file_path)
+    except requests.ConnectionError:
+        print('Failed to Save Image')
 if __name__ == "__main__":
     siteNum = int(input('请输入网站名：\n1:caribbeancom, 2:caribbeancompr, 3:heyzo, 4:ken8tengoku, 5:1pondo, 6:R18, 7:tokyoHot\n'))-1
     id = input('请输入id：\n')
